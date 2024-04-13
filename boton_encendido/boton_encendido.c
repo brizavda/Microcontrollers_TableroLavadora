faf1fba7053cb814158956e8126f059f13e581c6
Gamaliel Abad <gamaliel.abad@cua.uam.mx>
2024-04-12 21:38:40 -0600

    Programar un led y un botón

--- boton_encendido/boton_encendido.c
+++ boton_encendido/boton_encendido.c
@@ -1,1 +1,54 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
 
+#define LED_PIN 3
+#define BOTON_PRESS 4
+
+int main(){
+
+    // Variable para almacenar el estado de la lavadora
+    bool estado_lavadora = false; 
+
+    // Inicializar la salida estándar
+    stdio_init_all();
+
+    // Inicializar los pines GPIO para el LED y el botón
+    gpio_init(LED_PIN);
+    gpio_init(BOTON_PRESS);
+
+    // Establecer la dirección de los pines GPIO
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_set_dir(BOTON_PRESS, GPIO_IN);
+    
+    // Activar resistencia pull-up para el botón
+    gpio_pull_up(BOTON_PRESS);
+
+    // Bucle principal
+    while(true){
+        
+        // Parpadear el LED brevemente
+        gpio_put(LED_PIN, 1);
+        sleep_ms(1);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(1); 
+
+        // Apagar el LED durante un tiempo más largo (simulando pausa entre parpadeos)
+        gpio_put(LED_PIN, 0);
+        sleep_ms(100); 
+        
+        // Leer el estado del botón de presión
+        if (gpio_get(BOTON_PRESS) == 1){
+            // Si el botón está presionado, la lavadora está apagada
+            printf("Lavadora apagada\n");
+            sleep_ms(100);
+        } else {
+            // Si el botón no está presionado, la lavadora está encendida
+            printf("Lavadora encendida\n");
+            sleep_ms(100);
+        } 
+    }
+    
+    // Nunca se llega aquí, pero el estándar exige que main() devuelva un valor
+    return 0; 
+}
