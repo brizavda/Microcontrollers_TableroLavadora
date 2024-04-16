b55c7bec0d0607e8081930e09577d0c6f312a65e
Rodrigo Zamudio Tovar <royzamudiotovar@gmail.com>
2024-04-16 13:04:42 -0600

    Declaración de cabeceras y defines

---
+++ DisplayFases/display_fases.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+
+#define PRIMER_GPIO 2
+#define BOTON_GPIO1 11 // Botón para la animación circular
+#define BOTON_GPIO2 12 // Botón para la animación de infinito
+#define BOTON_GPIO3 13 // Botón para la animación 'S'
+
+//Arreglos para definir las diferentes secuencias para cada botón
+
+int main(){
+    
+}
