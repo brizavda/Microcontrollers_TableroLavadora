e518f6434482fcff5da4fc723dd2ee3d5d80b0e3
Rodrigo Zamudio Tovar <royzamudiotovar@gmail.com>
2024-04-16 13:06:57 -0600

    Inicializar arreglos y bucle for

--- DisplayFases/display_fases.c
+++ DisplayFases/display_fases.c
@@ -8,7 +8,19 @@
 #define BOTON_GPIO3 13 // Botón para la animación 'S'
 
 //Arreglos para definir las diferentes secuencias para cada botón
+int secLavar[4] = {0x01, 0x22, 0x14, 0x08};                             // Secuencia como en cascada
+int secEnjuagar[8] = {0x01, 0x02, 0x40, 0x10, 0x08, 0x04, 0x40, 0x20};  // Secuencia de animación de infinito
+int secCentrifugar[6] = {0x01, 0x02, 0x04, 0x08, 0x10, 0x20};            // Secuencia de animación circular
 
 int main(){
+    stdio_init_all();
     
+    printf("Control de secuencia en display de 7 segmentos\n");
+
+    // Configurar GPIOs para segmentos y botones
+    for (int gpio = PRIMER_GPIO; gpio < PRIMER_GPIO + 7; gpio++) {
+        gpio_init(gpio);
+        gpio_set_dir(gpio, GPIO_OUT);
+        gpio_set_outover(gpio, GPIO_OVERRIDE_INVERT);
+    }
 }
