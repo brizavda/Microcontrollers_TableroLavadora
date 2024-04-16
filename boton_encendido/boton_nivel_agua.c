d7d512570eba61a77c521fca05cfe571c7093178
Gamaliel Abad <gamaliel.abad@cua.uam.mx>
2024-04-15 23:13:23 -0600

    Se agregaron los sleep_ms y también se mejoró la logica del programa ademas se comprobó que el prototipo 1 funcionara

--- boton_encendido/boton_nivel_agua.c
+++ boton_encendido/boton_nivel_agua.c
@@ -19,55 +19,71 @@ int main()
 
     bool cambio_de_nivel = false;
 
-    while (gpio_get(BOTON_PRESS) == 0)
+    while (true)
     {
-        cambio_de_nivel = true;
-    }
+        
+        while (gpio_get(BOTON_PRESS) == 0)
+        {
+            cambio_de_nivel = true;
+            sleep_ms(100);
+        }
 
-    if (cambio_de_nivel == true)
-    {
+        if (cambio_de_nivel == true)
+        {
+            if (nivel_agua == 0)
+            {
+                nivel_agua = 1;
+                sleep_ms(100);
+            }
+            else if (nivel_agua == 1)
+            {
+                nivel_agua = 2;
+                sleep_ms(100);
+            }
+            else if (nivel_agua == 2)
+            {
+                nivel_agua = 3;
+                sleep_ms(100);
+            }
+            else if (nivel_agua == 3)
+            {
+                nivel_agua = 4;
+                sleep_ms(100);
+            }
+            else
+            {
+                nivel_agua = 1;
+                sleep_ms(100);
+            }
+        }
+
+        cambio_de_nivel = false;
+        sleep_ms(50);
+
         if (nivel_agua == 0)
         {
-            nivel_agua = 1;
+            printf("Selecciona el nivel de agua\n");
+            sleep_ms(50);
         }
         else if (nivel_agua == 1)
         {
-            nivel_agua = 2;
+            printf("Nivel de agua bajo\n");
+            sleep_ms(50);
         }
         else if (nivel_agua == 2)
         {
-            nivel_agua = 3;
+            printf("Nivel de agua medio\n");
+            sleep_ms(50);
         }
         else if (nivel_agua == 3)
         {
-            nivel_agua = 4;
+            printf("Nivel de agua medio alto\n");
+            sleep_ms(50);
         }
         else
         {
-            nivel_agua = 1;
+            printf("Nivel de agua alto\n");
+            sleep_ms(50);
         }
-    }
-
-    cambio_de_nivel = false;
-
-    if (nivel_agua == 0)
-    {
-        printf("Selecciona el nivel de agua");
-    }
-    else if (nivel_agua == 1)
-    {
-        printf("Nivel de agua bajo");
-    }
-    else if (nivel_agua == 2)
-    {
-        printf("Nivel de agua medio");
-    }
-    else if (nivel_agua == 3)
-    {
-        printf("Nivel de agua medio alto");
-    }
-    else
-    {
-        printf("Nivel de agua alto");
     }
 }
