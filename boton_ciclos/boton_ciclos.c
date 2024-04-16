4b346104edf1f17a72ec41235767314100328a74
brizavda <italia.barron@cua.uam.mx>
2024-04-16 13:12:21 -0600

    Agregar evento boton presionado

--- boton_ciclos/boton_ciclos.c
+++ boton_ciclos/boton_ciclos.c
@@ -5,8 +5,24 @@
 #define BOTON_PRESS 4
 
 int main(){
+
     stdio_init_all();
+    printf("Ciclos de lavadora - Normal(1), Delicado(2), Rápido(3)\n");
+
     gpio_init(BOTON_PRESS);
     gpio_set_dir(BOTON_PRESS, GPIO_IN);
     gpio_pull_up(BOTON_PRESS);
+
+    int ciclos = 0;
+    
+    bool button_pressed = false;
+
+    while(true){
+        while (gpio_get(BOTON_PRESS) == 0)
+        {
+            cambio_de_nivel = true;
+            sleep_ms(100);
+        }
+    }
+
 }
