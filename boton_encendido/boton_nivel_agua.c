8557a6aa4207acf860c3ba92b9efaf78c56d9ecb
Gamaliel Abad <gamaliel.abad@cua.uam.mx>
2024-04-15 22:39:33 -0600

    Se programó el primer prototipo para la seleccion de nivel de agua

--- boton_encendido/boton_nivel_agua.c
+++ boton_encendido/boton_nivel_agua.c
@@ -2,10 +2,10 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 
-
 #define BOTON_PRESS 4
 
-int main(){
+int main()
+{
 
     stdio_init_all();
 
@@ -15,17 +15,59 @@ int main(){
 
     gpio_pull_up(BOTON_PRESS);
 
-    int main(){
+    int nivel_agua = 0;
 
-        int nivel_agua;
-         
+    bool cambio_de_nivel = false;
 
-
+    while (gpio_get(BOTON_PRESS) == 0)
+    {
+        cambio_de_nivel = true;
     }
 
+    if (cambio_de_nivel == true)
+    {
+        if (nivel_agua == 0)
+        {
+            nivel_agua = 1;
+        }
+        else if (nivel_agua == 1)
+        {
+            nivel_agua = 2;
+        }
+        else if (nivel_agua == 2)
+        {
+            nivel_agua = 3;
+        }
+        else if (nivel_agua == 3)
+        {
+            nivel_agua = 4;
+        }
+        else
+        {
+            nivel_agua = 1;
+        }
+    }
 
+    cambio_de_nivel = false;
 
-      
-
-
+    if (nivel_agua == 0)
+    {
+        printf("Selecciona el nivel de agua");
+    }
+    else if (nivel_agua == 1)
+    {
+        printf("Nivel de agua bajo");
+    }
+    else if (nivel_agua == 2)
+    {
+        printf("Nivel de agua medio");
+    }
+    else if (nivel_agua == 3)
+    {
+        printf("Nivel de agua medio alto");
+    }
+    else
+    {
+        printf("Nivel de agua alto");
+    }
 }
