6a796a3a8c5026d3e31b35af4f5b96ac04238ca8
Rodrigo Zamudio Tovar <royzamudiotovar@gmail.com>
2024-04-21 12:34:20 -0600

    Declarar cabeceras y defines

---
+++ PruebaAnimBarrido/prueba_anim_barrido.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+
+#define FIRST_GPIO 0
+#define COMMUN_1 8
+#define COMMUN_2 9
+#define COMMUN_3 10
+#define COMMUN_4 11
+#define BUTTON_GPIO 13
+#define LED_PIN 14
