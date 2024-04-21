ec586d5f7b951fcf6eac578a85e72c72efcb7a65
Rodrigo Zamudio Tovar <royzamudiotovar@gmail.com>
2024-04-21 15:47:03 -0600

    Configurar GPIOs

--- AnimCiclosLavado/ciclos_anim.c
+++ AnimCiclosLavado/ciclos_anim.c
@@ -7,3 +7,19 @@
 
 // Arreglo para definir la secuencia base para todos los ciclos
 int secCiclos[5] = {0x01, 0x20, 0x40, 0x04, 0x08}; // Secuencia en forma de "S"
+
+
+int main(){
+    stdio_init_all();
+    printf("Ciclos de lavadora - Normal(1), Delicado(2), Rápido(3)\n");
+
+    gpio_init(BOTON_PRESS);
+    gpio_set_dir(BOTON_PRESS, GPIO_IN);
+    gpio_pull_up(BOTON_PRESS);
+
+    // Configurar GPIOs para segmentos del display
+    for (int gpio = FIRST_GPIO; gpio < FIRST_GPIO + 7; gpio++) {
+        gpio_init(gpio);
+        gpio_set_dir(gpio, GPIO_OUT);
+    }
+}
