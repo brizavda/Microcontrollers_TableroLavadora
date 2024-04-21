02acd2bffc82726a73b117d32a6f6e8f8067c9ce
Rodrigo Zamudio Tovar <royzamudiotovar@gmail.com>
2024-04-21 12:35:56 -0600

    Inicializar arreglos de palabras

--- PruebaAnimBarrido/prueba_anim_barrido.c
+++ PruebaAnimBarrido/prueba_anim_barrido.c
@@ -9,3 +9,17 @@
 #define COMMUN_4 11
 #define BUTTON_GPIO 13
 #define LED_PIN 14
+
+// Mapas de bits para mostrar las palabras "HOLA" y "BYE" en un display de 7 segmentos
+int palabra_HOLA[4] = {
+    0x76, // H
+    0x3F, // O
+    0x38, // L
+    0x77  // A
+};
+
+int palabra_BYE[3] = {
+    0x7C, // b
+    0x6E, // y
+    0x79  // e
+};
