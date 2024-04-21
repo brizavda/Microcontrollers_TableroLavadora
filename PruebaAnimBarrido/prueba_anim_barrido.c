1ee70f4d3b64d27fb82af7b17b96f9342a04ace4
Rodrigo Zamudio Tovar <royzamudiotovar@gmail.com>
2024-04-21 12:46:59 -0600

    Configurar pines

--- PruebaAnimBarrido/prueba_anim_barrido.c
+++ PruebaAnimBarrido/prueba_anim_barrido.c
@@ -28,10 +28,32 @@ int main(){
     stdio_init_all();
 
     // Configurar GPIOs para los segmentos del display
-    for (int gpio = FIRST_GPIO; gpio < FIRST_GPIO + 7; gpio++)
-    {
+    for (int gpio = FIRST_GPIO; gpio < FIRST_GPIO + 7; gpio++){
         gpio_init(gpio);
         gpio_set_dir(gpio, GPIO_OUT);
         gpio_put(gpio, 0); // Apagar todos los segmentos al inicio
     }
+
+    // Configurar pines de los dígitos comunes del display
+    gpio_init(COMMUN_1);
+    gpio_set_dir(COMMUN_1, GPIO_OUT);
+    gpio_put(COMMUN_1, 1); // Apagar el dígito común 1
+    gpio_init(COMMUN_2);
+    gpio_set_dir(COMMUN_2, GPIO_OUT);
+    gpio_put(COMMUN_2, 1); // Apagar el dígito común 2
+    gpio_init(COMMUN_3);
+    gpio_set_dir(COMMUN_3, GPIO_OUT);
+    gpio_put(COMMUN_3, 1); // Apagar el dígito común 3
+    gpio_init(COMMUN_4);
+    gpio_set_dir(COMMUN_4, GPIO_OUT);
+    gpio_put(COMMUN_4, 1); // Apagar el dígito común 4
+
+    // Configurar pin del botón como entrada con pull-up
+    gpio_init(BUTTON_GPIO);
+    gpio_set_dir(BUTTON_GPIO, GPIO_IN);
+    gpio_pull_up(BUTTON_GPIO);
+
+    // Configurar pin del LED como salida
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
 }
