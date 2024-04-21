b90ee0f10c5c87aedec2ce5edf9060a7c1fbe448
Rodrigo Zamudio Tovar <royzamudiotovar@gmail.com>
2024-04-21 15:45:05 -0600

    Inicializar arreglo de secuencia en S

--- AnimCiclosLavado/ciclos_anim.c
+++ AnimCiclosLavado/ciclos_anim.c
@@ -4,3 +4,6 @@
 
 #define BOTON_PRESS 1
 #define FIRST_GPIO 2
+
+// Arreglo para definir la secuencia base para todos los ciclos
+int secCiclos[5] = {0x01, 0x20, 0x40, 0x04, 0x08}; // Secuencia en forma de "S"
