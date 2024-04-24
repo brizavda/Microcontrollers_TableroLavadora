030af9bb819c8516f5f4af22bec537c802edce76
Rodrigo Zamudio Tovar <royzamudiotovar@gmail.com>
2024-04-24 17:47:44 -0600

    Declaración de atributos y métodos publicos y privados

--- boton_Fases/boton_fases.h
+++ boton_Fases/boton_fases.h
@@ -3,5 +3,16 @@
 #include "hardware/gpio.h"
 
 class BotonFases{
+    private:
+        int fase_actual;
+        int fase_anterior;
+        static uint32_t delay; // Tiempo de espera en microsegundos
+        
+    public:
+        BotonFases(); // Constructor
 
+        void configurarGPIOs();
+        void cambiarFase();
+        void mostrarFase();
+        void ejecutarSecuencia();
 };
