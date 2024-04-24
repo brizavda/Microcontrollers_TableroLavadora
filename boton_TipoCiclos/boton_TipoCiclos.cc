3a93dd21983e2bc76067622a5d22df6b5244834b
brizavda <italia.barron@cua.uam.mx>
2024-04-24 13:58:46 -0600

    Resolución de problemas con el CMake y carpetas

---
+++ boton_TipoCiclos/boton_TipoCiclos.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+
+#define BOTON_PRESS 26
+
+class{
+
+}
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -40,6 +40,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("C:/Users/brizi/Documents/Pico-v1.5.1/pico-trad/Microcontrollers_TableroLavadora/build/pico-sdk/cmake_install.cmake")
+  include("C:/Users/brizi/Documents/Pico-v1.5.1/pico-trad/Microcontrollers_TableroLavadora/build/boton_TipoCiclos/cmake_install.cmake")
 
 endif()
 
