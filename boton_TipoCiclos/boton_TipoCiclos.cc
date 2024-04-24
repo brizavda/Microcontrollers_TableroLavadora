4db510b2d9647773677ffcb2b7349291335a2e89
brizavda <italia.barron@cua.uam.mx>
2024-04-24 14:22:43 -0600

    Definicion de atributos y metodos

--- boton_TipoCiclos/boton_TipoCiclos.cc
+++ boton_TipoCiclos/boton_TipoCiclos.cc
@@ -5,6 +5,17 @@
 
 #define BOTON_PRESS 26
 
-class{
+class botonCiclos{
+    private:
+        bool btn_press = false;
+        int ciclos = 0;
 
+    public:
+        botonCiclos()
+        leer_boton()
+}
+
+int botonCiclos::leer_boton()
+{
+    return gpio_get(BOTON_PRESS);
 }
