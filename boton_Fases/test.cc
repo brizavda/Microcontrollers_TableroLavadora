665f062cd23c597efaa45fe5a2058903eeedce7b
Rodrigo Zamudio Tovar <royzamudiotovar@gmail.com>
2024-04-24 18:34:21 -0600

    Lógica de main

--- boton_Fases/test.cc
+++ boton_Fases/test.cc
@@ -5,4 +5,19 @@
 
 int main() {
     stdio_init_all();
+
+    BotonFases botonFases;
+    botonFases.configurarGPIOs(); // Configurar GPIOs necesarios
+
+    while (true) {
+        botonFases.cambiarFase(); // Cambiar la fase si se presiona el botón
+
+        // Mostrar fase seleccionada por serial
+        botonFases.mostrarFase();
+
+        // Ejecutar la secuencia de LED según la fase actual
+        botonFases.ejecutarSecuencia();
+
+        sleep_ms(50); // Pequeña pausa entre ciclos
+    }
 }
