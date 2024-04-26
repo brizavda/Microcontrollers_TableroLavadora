e15b2b0224b1134eb2bbae8450b131d21b6cd85f
brizavda <italia.barron@cua.uam.mx>
2024-04-25 21:34:25 -0600

    Agregar comentarios documentando las clases

--- boton_TipoCiclos/boton_TipoCiclo.h
+++ boton_TipoCiclos/boton_TipoCiclo.h
@@ -4,18 +4,32 @@
 
 class botonCiclos{
     private:
-        int cicloNormal[4];    /**@brief Arreglo que contiene la secuencia para la fase de lavado. */
-        int cicloDelicado[8]; /**@brief Arreglo que contiene la secuencia para la fase de enjuague. */
-        int cicloRapido[6];
-        int cicloActual;
-        int cicloAnterior;
-        static uint32_t delay; 
-        bool btnPress()
-
+        // Arreglo para definir la secuencia base para todos los ciclos
+        int tipoCiclos[5] = {0x01, 0x20, 0x40, 0x04, 0x08}; /**@brief Arreglo que contiene la secuencia en forma de "S". */
+        int cicloActual; /**@brief Tipo actual del ciclo de lavado. */
+        int cicloAnterior; /**< @brief Tipo anterior del ciclo de lavado. */
+        static uint32_t delay; /**@brief Tiempo de espera en microsegundos para la ejecución de la secuencia. */
+        /**
+         * @brief Verifica si el botónde tipo de ciclo está presionado.
+         * @return true el botón está presionado, false en caso contrario.
+         */
+        bool btnTiposPress(); 
     public:
-        botonCiclos()
+        botonCiclos() // Constructor
+        /**
+         * @brief Configura los GPIOs necesarios para el control de secuencias y botones.
+         */
         void inicializar()
+        /**
+         * @brief Cambia la fase actual si se detecta el botón presionado.
+         */
         void cambiarDisplay()
+         /**
+         * @brief Muestra la fase actual por el monitor serial.
+         */
         void mostrarDisplay()
+        /**
+         * @brief Ejecuta la secuencia en el display de 7 segmentos correspondiente al tipo actual.
+         */
         void seleccionCiclo()
 }
