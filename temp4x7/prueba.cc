5f692dd08bef4356508454b53855259357619c5a
Rodrigo Zamudio Tovar <royzamudiotovar@gmail.com>
2024-04-30 11:38:37 -0600

    Construcción de prueba

---
+++ temp4x7/prueba.cc
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "boton_temporizador.h"
+#include "../include/pin_list.h"
+
+int main() {
+    stdio_init_all();
+
+    // Crear instancias de los botones
+    BotonTemporizador botonTemporizador(BOTON_TEMP);
+
+    // Inicializar los botones
+    botonTemporizador.inicializar();
+
+    int minutos = 0;
+    int segundos = 0;
+
+    while (true) {
+        // Mostrar el nivel de agua seleccionado
+        // Esto es opcional, solo para propósitos de demostración
+        // printf("Nivel de agua seleccionado: %d\n", nivelAgua);
+
+        // Aumentar el tiempo del temporizador si se presiona el botón
+        botonTemporizador.aumentar_tiempo(minutos, segundos);
+
+        // Mostrar el tiempo del temporizador
+        // Esto es opcional, solo para propósitos de demostración
+        // printf("Tiempo del temporizador: %02d:%02d\n", minutos, segundos);
+
+        sleep_ms(200); // Pequeña pausa entre iteraciones
+    }
+
+    return 0;
+}
