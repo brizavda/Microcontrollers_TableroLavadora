4305ef396f1602233ea6aa3d58f3625919efa71d
Italia Barrón <141947806+brizavda@users.noreply.github.com>
2024-05-24 19:43:07 +0000

    Implementación del test para el sensor de proximidad

---
+++ sensor_Proximidad/test.py
@@ -0,0 +1,19 @@
+import utime
+from sensor_Ultrasonic import init_ultrasonic, measure_distance
+
+def main():
+    # Initialize GPIOs
+    trigger_pin = 15
+    echo_pin = 14
+    trigger, echo = init_ultrasonic(trigger_pin, echo_pin)
+    
+    while True:
+        # Measure distance
+        distancia = measure_distance(trigger, echo)
+        print("Distancia:", distancia, "cm")
+        
+        # Delay for x seconds before next measurement
+        utime.sleep(1)
+
+if __name__ == "__main__":
+    main()
