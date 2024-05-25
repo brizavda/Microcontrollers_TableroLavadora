57c845e708316a25af6239f05f5ce0b2714eefda
Rodrigo Zamudio Tovar <royzamudiotovar@gmail.com>
2024-05-24 19:46:16 -0600

    Probar main y melodias mediante una selección en la consola

--- test_zumbador.py
+++ test_zumbador.py
@@ -1,1 +1,33 @@
-# Achivo para probar la reproducción de las melodias
+import time
+import melodias
+
+def seleccionar_melodia():
+    print("Seleccione una melodía:")
+    print("1: Melodía de Inicio")
+    print("2: Melodía de Finalización")
+    print("3: Melodía de Apagado")
+    print("4: Melodía de Pausa")
+    opcion = input("Ingrese el número de la opción deseada: ")
+
+    if opcion == '1':
+        print("Reproduciendo Melodía de Inicio...")
+        melodias.melodia_inicio()
+    elif opcion == '2':
+        print("Reproduciendo Melodía de Finalización...")
+        melodias.melodia_final()
+    elif opcion == '3':
+        print("Reproduciendo Melodía de Apagado...")
+        melodias.melodia_apagado()
+    elif opcion == '4':
+        print("Reproduciendo Melodía de Pausa...")
+        melodias.melodia_pausa()
+    else:
+        print("Opción no válida. Por favor, intente nuevamente.")
+
+def main():
+    while True:
+        seleccionar_melodia()
+        time.sleep(1)
+
+if __name__ == "_main_":
+    main()
