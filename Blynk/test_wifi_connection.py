f8b3a0449afc8dc39d9b78299d81b7e526e495c1
Cande99 <125041471+Cande99@users.noreply.github.com>
2024-05-28 08:53:29 -0600

    Update test_wifi_connection.py

--- Blynk/test_wifi_connection.py
+++ Blynk/test_wifi_connection.py
@@ -1,48 +1,11 @@
-import time
-import network
-from machine import Pin, UART
-import BlynkLib
+import blynkHandler
 
-# Configuración de la red WiFi
-SSID = "INFINITUME7DF_2.4" # Nombre de tu WiFi
-PASSWORD = "2ftva4Nazn" # Contraseña de tu WiFi
-BLYNK_AUTH = "S7taOmoMRuVzJWM7wrT9QSHhDHbx74tb" # Token de Blynk
+def main():
+    # Inicializar la conexión WiFi y Blynk
+    blynk = blynkHandler.init()
+    
+    # Ejecutar el bucle principal de Blynk
+    blynkHandler.run_blynk(blynk)
 
-# Conexión a la red WiFi
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-wlan.connect(SSID, PASSWORD)
-
-# Espera a que se establezca la conexión
-max_wait = 10
-while max_wait > 0:
-    if wlan.isconnected():
-        break
-    print('Esperando conexión...')
-    time.sleep(1)
-    max_wait -= 1
-
-if not wlan.isconnected():
-    raise RuntimeError('Falló la conexión a la red')
-else:
-    print('Conectado')
-    ip = wlan.ifconfig()[0]
-    print('IP:', ip)
-
-# Conexión a Blynk
-blynk = BlynkLib.Blynk(BLYNK_AUTH)
-
-# Configuración UART
-uart = UART(0, baudrate=9600, tx=16, rx=17)
-
-# Manejador para el pin virtual V1
-@blynk.on("V1")
-def v1_write_handler(value):
-    if int(value[0]) == 1:
-        uart.write('ON\n')
-    else:
-        uart.write('OFF\n')
-
-# Ciclo principal
-while True:
-    blynk.run()
+if __name__ == "__main__":
+    main()
