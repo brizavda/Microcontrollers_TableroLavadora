011e2354f0e613377f9d584f7694d1bec601741f
Gamaliel Abad <gamaliel.abad@cua.uam.mx>
2024-05-27 20:53:12 -0600

    Se modularizó el sensor de nivel de agua

--- Sensor_nivel_agua/Sensor_nivel_agua.py
+++ Sensor_nivel_agua/Sensor_nivel_agua.py
@@ -1,20 +1,22 @@
 from machine import ADC 
 from time import sleep
 
-sensor_pin = ADC(26)  # Usamos el pin A0 en ESP8266
-
-while True:
-    # Lee el valor analógico del sensor
-    valor_analogico = sensor_pin.read_u16()
-    print(valor_analogico)
+class SensorNivelAgua():
     
-    if valor_analogico < 500:
-        print("Sin agua")
-    elif valor_analogico > 500 and valor_analogico < 30000:
-        print("Nivel de agua bajo")
-    elif valor_analogico > 30001 and valor_analogico < 32000: 
-        print("Valor de agua media")
-    else: 
-        print("Nivel de agua lleno")
+    def __init__(self, x: int) -> None:
+        self.sensor_pin = ADC(x)
+        
+    def lectura_valor(self) -> str:
+        
+        valor_analogico = self.sensor_pin.read_u16()
+        print(valor_analogico)
     
-    sleep(1)  # Espera 1 segundo antes de la siguiente lectura
+        if valor_analogico < 500:
+            return "Sin agua"
+        elif valor_analogico > 500 and valor_analogico < 30000:
+            return "Nivel de agua bajo"
+        elif valor_analogico > 30001 and valor_analogico < 32000: 
+            return "Valor de agua media"
+        else: 
+            return "Nivel de agua lleno"
+            
