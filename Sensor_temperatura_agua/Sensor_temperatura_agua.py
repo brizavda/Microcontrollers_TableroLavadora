34289063718700d4c9def6631e82e7b45f474a43
Gamaliel Abad <gamaliel.abad@cua.uam.mx>
2024-05-27 18:50:42 -0600

    Ya funciona el sensor de la temperatura del agua, faltan pruebas

--- Sensor_temperatura_agua/Sensor_temperatura_agua.py
+++ Sensor_temperatura_agua/Sensor_temperatura_agua.py
@@ -1,32 +1,15 @@
 from machine import Pin
+from time import sleep
+
 import onewire
 from ds18x20 import DS18X20
-from time import sleep
 
 import binascii
 
-ow = onewire.OneWire(Pin(4)) #Prepara GPIO4 para usar con OneWire
+ow = onewire.OneWire(Pin(4),) 
 
-sensor = DS18X20(ow) #define un sensor en ese pin
+sensor = DS18X20(ow) 
 
-direcciones = sensor.scan()  #Lee el ID del sensor conectado
-id=direcciones[0]
+direcciones = sensor.scan()
+id = direcciones[0]
 
-#Pasa el ID a formato de texto e imprime
-idHex = binascii.hexlify(bytearray(id)) 
-print ("ID=",idHex)
-
-#Lee e imprime la temperatura cada 1 segundo
-while (True):
-    
-    try:
-        sensor.convert_temp ()
-        sleep (1)
-        temperatura = sensor.read_temp (id)
-        
-    except:
-        print ("Error en sensor!")
-        sleep (1)
-        
-    else:
-        print (temperatura)
